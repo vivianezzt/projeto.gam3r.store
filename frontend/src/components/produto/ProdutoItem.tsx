fb96b3c75bb7c918afcdaa4340c24cc8ef327b16
Viviane <vivianeaguiarc@outlook.com>
2024-10-07 21:06:30 -0300

    Criação dos components template, produto com produtoItem adicionando configurações, editando arquivo global, criação da pasta produto/[id], estilizando a parte de produtos da aplicação, aplicando as imagens dos produtos

--- frontend/src/components/produto/ProdutoItem.tsx
+++ frontend/src/components/produto/ProdutoItem.tsx
@@ -1,4 +1,5 @@
 import { Produto } from "@/core"
+import Image from "next/image"
 import Link from "next/link"
 
 export interface ProdutoItemProps {
@@ -8,8 +9,15 @@ export interface ProdutoItemProps {
 export default function ProdutoItem(props: ProdutoItemProps){
     const { produto } = props
     return <Link href={`/produto/${props.produto.id}`}
-                className="flex flex-col bg-violet-dark border border-white/10"
+                className="flex flex-col bg-violet-dark border border-white/10 rounded-xl relative max-w-[350px]"
             >
-            {produto.nome}
+            <div className="w-full h-48 relative">
+                <Image
+                    src={produto.imagem}
+                    fill
+                    className="object-contain"
+                    alt="Imagem do Produto"
+                />
+                </div>
             </Link>
 }
